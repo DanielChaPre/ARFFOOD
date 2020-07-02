7a59d41a3fdfa82222261ae60c0fec86687863ea
UTL02302061 <65818584+UTL02302061@users.noreply.github.com>
2020-07-02 13:13:18 -0500

    Actualización 20200702

---
+++ ARFood/Services/ARFoodServices.cs
@@ -0,0 +1,36 @@
+﻿using ARFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ARFood.Services
+{
+    public class ARFoodServices
+    {
+        ApplicationDbContext contex;
+        public ARFoodServices()
+        {
+            this.contex = new ApplicationDbContext();
+        }
+        public List<Familias> GetFamilias()
+        {
+            return this.contex.Familias.ToList();
+        }
+        public List<Productos> GetProductos(int Familia)
+        {
+            var consulta = from datos in contex.Productos
+                           where Familia.Equals(datos.Familia)
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public List<Productos> BuscarProductos(List<int> Productos)
+        {
+            var consulta = from datos in contex.Productos
+                           where Productos.Contains(datos.ID)
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
